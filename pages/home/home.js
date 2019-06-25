8f446b220429d85d7e87e01b8713bbc07e84643f
LinhuHe <2351855940@qq.com>
2019-06-25 15:03:11 +0800

    tag2

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,20 @@
 // pages/home/home.js
 Page({
+  data:{
+    name : "123456",
+    students : [
+      {id:1,name: "a"},
+      {id:2,name: "b"}
+    ],
+    counter: 0
+  },
+  counteradd()
+  {
+    this.setData({
+      counter: this.data.counter+1
+
+    })
+  }
+
 
 })
